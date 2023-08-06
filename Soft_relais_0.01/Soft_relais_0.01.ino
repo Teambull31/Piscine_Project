40ad38be710b40b9fa24863b899cb1e7abef7dea
Teambull31 <84913627+Teambull31@users.noreply.github.com>
2023-08-06 22:07:40 +0200

    personalize code variable and set timer value 10 min

--- Soft_relais_0.01/Soft_relais_0.01.ino
+++ Soft_relais_0.01/Soft_relais_0.01.ino
@@ -1,31 +1,34 @@
 // Complete Instructions: https://RandomNerdTutorials.com/esp32-digital-inputs-outputs-arduino/
 
 // set pin numbers
-const int buttonPin = 4;  // the number of the pushbutton pin
-const int ledPin =  15;    // the number of the LED pin
+const int Levelsensor = 4;  // the number of the pushbutton pin
+const int electrovanne =  15;    // the number of the LED pin
 
 // variable for storing the pushbutton status 
-int buttonState = 0;
+int WaterState = 0;
 
 void setup() {
   Serial.begin(115200);  
   // initialize the pushbutton pin as an input
-  pinMode(buttonPin, INPUT);
+  pinMode(Levelsensor, INPUT);
   // initialize the LED pin as an output
-  pinMode(ledPin, OUTPUT);
+  pinMode(electrovanne, OUTPUT);
 }
 
 void loop() {
   // read the state of the pushbutton value
-  buttonState = digitalRead(buttonPin);
-  Serial.println(buttonState);
+  WaterState = digitalRead(Levelsensor);
+  Serial.println(WaterState);
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH
-  if (buttonState == HIGH) {
+  if (WaterState == HIGH) {
     // turn LED on
-    digitalWrite(ledPin, LOW);
-  } else {
+    digitalWrite(electrovanne, HIGH);
+  } else if ( WaterState == LOW  )  {
     // turn LED off
-    digitalWrite(ledPin, HIGH);   
+    delay (600000); 
+    digitalWrite(electrovanne, LOW); 
+    
   }
+  //else if ()// if received 0x24 via Lora.
 }
