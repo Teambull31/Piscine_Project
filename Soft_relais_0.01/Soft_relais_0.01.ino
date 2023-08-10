b5fdf2682d39066fcef95afd85b2d550f83d1954
Mathéo Cozzolino <cozzolm@gmail.com>
2023-08-10 08:23:29 +0200

    Replace Delay by Millis

--- Soft_relais_0.01/Soft_relais_0.01.ino
+++ Soft_relais_0.01/Soft_relais_0.01.ino
@@ -3,7 +3,9 @@
 // set pin numbers
 const int Levelsensor = 4;  // the number of the pushbutton pin
 const int electrovanne =  15;    // the number of the LED pin
-
+unsigned long dynamique_interval = 60000;
+const long interval = dynamique_interval;
+unsigned long previousMillis = 0;
 // variable for storing the pushbutton status 
 int WaterState = 0;
 
@@ -18,15 +20,17 @@ void setup() {
 void loop() {
   // read the state of the pushbutton value
   WaterState = digitalRead(Levelsensor);
+  unsigned long currentMillis = millis();
   Serial.println(WaterState);
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH
   if (WaterState == HIGH) {
     // turn LED on
     digitalWrite(electrovanne, HIGH);
-  } else if ( WaterState == LOW  )  {
+  } else if ( WaterState == LOW && (currentMillis - previousMillis > interval) )  {
     // turn LED off
-    delay (600000); 
+     previousMillis = currentMillis;
+    //delay (600000); 
     digitalWrite(electrovanne, LOW); 
     
   }
